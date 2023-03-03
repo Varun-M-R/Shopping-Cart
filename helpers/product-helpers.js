ace7f4c6f6e8b32b4facd0bdea95470109d51914
Varun <varunmr619@gmail.com>
2023-03-03 14:53:39 +0530

    image in product-images folder

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -3,7 +3,8 @@ module.exports={
     addProduct:(product,callback)=>{
         console.log(product);
         db.get().collection('product').insertOne(product).then((data)=>{
-         callback(true)
+          //console.log("hiiiii"+JSON.stringify(data));  
+         callback(data.insertedId)
         })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -40,8 +40,19 @@ router.post('/add-product',(req,res)=>{
   console.log(req.body);
   console.log(req.files.Image);
   
-  productHelper.addProduct(req.body,(result)=>{
-    res.render("admin/add-product")
+  productHelper.addProduct(req.body,(id)=>{
+    let image=req.files.Image
+    console.log(id);
+    image.mv('./public/product-images/'+id+".jpg",(err,done)=>{
+     if(!err){
+      res.render("admin/add-product")
+     } else{
+      console.log(err);
+     }
+
+
+    })
+   
     
   })
 })
